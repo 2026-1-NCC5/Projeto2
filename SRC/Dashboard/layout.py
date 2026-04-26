336c3d0424dd5df178e3ea7c493723ae66ea2bb8
AntonioPetri <antonio-petri@hotmail.com>
2026-04-26 14:19:54 -0300

    f

--- SRC/Dashboard/layout.py
+++ SRC/Dashboard/layout.py
@@ -2,9 +2,12 @@ from dash import html, dcc
 import plotly.io as pio
 import plotly.graph_objects as go
 
-# Fonte Arial como padrão para todos os gráficos Plotly
+# Fonte Arial como padrão para todos os gráficos Plotly (labels, eixos e tooltips)
 pio.templates["arial"] = go.layout.Template(
-    layout=dict(font=dict(family="Arial, Helvetica, sans-serif"))
+    layout=dict(
+        font=dict(family="Arial, Helvetica, sans-serif"),
+        hoverlabel=dict(font=dict(family="Arial, Helvetica, sans-serif")),
+    )
 )
 pio.templates.default = "plotly+arial"
 
